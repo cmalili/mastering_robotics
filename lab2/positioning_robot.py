948ca323e62f0242759014a54853b0682742041c
Christopher, Malili <malilichristopher@gmail.com>
2025-09-16 19:53:23 -0700

    Created positioning_robot.py file in lab2

---
+++ lab2/positioning_robot.py
@@ -0,0 +1,17 @@
+from serial.tools import list_ports
+
+from pydobot import Dobot
+
+
+port = list_ports.comports()[0].device
+device = Dobot(port=port)
+
+pose = device._get_pose()
+print(pose)
+position = pose.position
+
+device.move_to(position.x + 20, position.y, position.z, position.r, wait=False)
+device.move_to(position.x, position.y, position.z, position.r, wait=True)
+
+
+device.close()
