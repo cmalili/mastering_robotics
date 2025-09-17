dfd61201ceda451308aecd71dc3ebfd2a734706a
Christopher, Malili <malilichristopher@gmail.com>
2025-09-16 19:53:23 -0700

    Created positioning_robot.py file in lab2

--- lab2/positioning_robot.py
+++ lab2/positioning_robot.py
@@ -6,7 +6,11 @@ from pydobot import Dobot
 port = list_ports.comports()[0].device
 device = Dobot(port=port)
 
+<<<<<<< HEAD
 pose = device.get_pose()
+=======
+pose = device._get_pose()
+>>>>>>> 88cd7d2b (Created positioning_robot.py file in lab2)
 print(pose)
 position = pose.position
 
